077cd642b454740c2c64078d6fcad5a63e322ed9
avraL <38397667+avr4L@users.noreply.github.com>
2019-03-18 14:03:49 +0100

    Add files via upload

---
+++ 01_assembly_primer_for_hackers/UnconditionalBranching.s
@@ -0,0 +1,31 @@
+.data
+	HelloWorldString:
+		.asciz "Hello World!"
+	CallDemo:
+		.asciz "Call works!"
+
+.text
+	.globl _start
+
+	_start:
+
+		# Write HelloWorld
+        movl $4, %eax
+        movl $1, %ebx
+		movl $HelloWorldString, %ecx
+        movl $12, %edx
+        int $0x80
+        
+    ExitProgram:
+        #Exit the program
+        movl $1, %eax
+        movl $10, %ebx
+        int $0x80
+
+    CallMe:
+        movl $4, %eax
+        movl $1, %ebx
+        movl $CallDemo, %ecx
+        movl $11, %edx
+        int $0x80
+        ret
---
+++ 01_assembly_primer_for_hackers/stack.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int AddMe(int a, int b)
+{
+    int c;
+    c = a + b;
+    return c;
+}
+
+void main()
+{
+    AddMe(10, 20);
+    printf("Hello World");
+}
